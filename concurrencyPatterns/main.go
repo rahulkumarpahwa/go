a50a7b2b50aca77037fafad1e2567937e9f84581
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-05 21:15:06 +0530

    concurrency pattern : fan out using the channel

--- concurrencyPatterns/main.go
+++ concurrencyPatterns/main.go
@@ -6,24 +6,33 @@ import (
 	"time"
 )
 
-func imageProcessingWorker(imageUrl string, wg *sync.WaitGroup) {
+func imageProcessingWorker(imageUrl string, wg *sync.WaitGroup, resultChannel chan string) {
 	defer wg.Done()
 	time.Sleep(time.Millisecond * 50) // trying to mimic the time taken to process the image.
 	fmt.Println("image Processed : ", imageUrl)
+	resultChannel <- imageUrl
 }
 
 func main() {
-
-	startTime := time.Now()
 	var wg sync.WaitGroup
+	resultChannel := make(chan string, 5)
+	startTime := time.Now()
 
 	wg.Add(5)
-	go imageProcessingWorker("image1.png", &wg)
-	go imageProcessingWorker("image2.png", &wg)
-	go imageProcessingWorker("image3.png", &wg)
-	go imageProcessingWorker("image4.png", &wg)
-	go imageProcessingWorker("image5.png", &wg)
+	go imageProcessingWorker("image1.png", &wg, resultChannel)
+	go imageProcessingWorker("image2.png", &wg, resultChannel)
+	go imageProcessingWorker("image3.png", &wg, resultChannel)
+	go imageProcessingWorker("image4.png", &wg, resultChannel)
+	go imageProcessingWorker("image5.png", &wg, resultChannel)
 
 	wg.Wait()
+	close(resultChannel) // channel always need to be closed, otherwise there will be deadlock.
+
+	for val := range resultChannel {
+		fmt.Println("Result of the Channel : ", val)
+	}
+
 	fmt.Printf("Estimated Time taken : %v", time.Since(startTime))
+
+	// here we have sended the string in the channel but in actual we will send the struct or a map or even a slice.
 }
