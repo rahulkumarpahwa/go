1b357c575d945041540865246e93374de24336ee
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-04-20 22:20:12 +0530

    Add initial implementation of TUI with Bubble Tea, including model and view logic

--- TotionTUI/main.go
+++ TotionTUI/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
@@ -9,24 +10,36 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	tea "charm.land/bubbletea/v2"
+	"github.com/rahulkumarpahwa/go/TotionTUI/internal/bubble"
 )
 
 func main() {
+
+	p := tea.NewProgram(bubble.Initialization())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
 	// config
 
 	// setting up the router
 	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is available!"))
+	})
 
 	// routes
 
 	// Listener and Serve
 	server := http.Server{
-		Addr:        "",
+		Addr:        "localhost:8080",
 		Handler:     router,
 		ReadTimeout: time.Duration(time.Second * 20),
 		IdleTimeout: time.Duration(time.Second * 20),
 	}
-
 	// Shutdown
 	signalChannel := make(chan os.Signal, 1)
 
@@ -34,18 +47,19 @@ func main() {
 
 	go func() {
 		err := server.ListenAndServe()
-		if err != nil {
+		if err != nil && err != http.ErrServerClosed { 
+			// http.ErrServerClosed is the general error which ListenAndServe gives on closing just to show it is closing.
 			log.Fatalf("Server didn't start, %v", err)
 		}
 	}()
 
 	<-signalChannel
 
-	slog.Info("~Shutdown of the server started~")
+	slog.Info("~ Shutdown of the server started ~")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	server.Shutdown(ctx)
-	slog.Info("~Server Shutdown Successfully~")
+	slog.Info("~ Server Shutdown Successfully ~")
 
 }
