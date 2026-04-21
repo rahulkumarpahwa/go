33faae36cef6f5abe6b349858b121a2765c18bcd
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-04-21 06:56:08 +0530

    Add lipgloss dependency and implement styling in bubbletea view

--- TotionTUI/internal/bubble/bubbletea.go
+++ TotionTUI/internal/bubble/bubbletea.go
@@ -1,9 +1,10 @@
 package bubble
 
 import (
-	"fmt"
+	// "fmt"
 
 	tea "charm.land/bubbletea/v2"
+	lipgloss "charm.land/lipgloss/v2"
 )
 
 type model struct {
@@ -33,7 +34,7 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
-	// Is it a key press? 
+	// Is it a key press?
 	case tea.KeyPressMsg:
 
 		// Cool, what was the actual key pressed?
@@ -73,30 +74,51 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
+
+	// Styling using the lipglose
+	var style = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Background(lipgloss.Color("#7c12e0")).
+		PaddingLeft(2).PaddingBottom(0).PaddingTop(0).
+		PaddingRight(2).Align(lipgloss.Center)
+
 	// The header
 	s := "Welcome to TUI based Notes App!\n\n"
 
+	// rendering the style
+	s = style.Render(s)
+
 	// Iterate over our choices
-	for i, choice := range m.choices {
+	// for i, choice := range m.choices {
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
+	// 	// Is the cursor pointing at this choice?
+	// 	cursor := " " // no cursor
+	// 	if m.cursor == i {
+	// 		cursor = ">" // cursor!
+	// 	}
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
+	// 	// Is this choice selected?
+	// 	checked := " " // not selected
+	// 	if _, ok := m.selected[i]; ok {
+	// 		checked = "x" // selected!
+	// 	}
 
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %d %s\n", cursor, checked, i+1, choice)
-	}
+	// 	// Render the row
+	// 	s += fmt.Sprintf("%s [%s] %d %s\n", cursor, checked, i+1, choice)
+	// }
 
+	s += "\n\n"
+
+	style = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#f91616")).Padding(0, 1, 0).Border(lipgloss.ASCIIBorder())
 	// The footer
-	s += "\nPress q to quit.\n"
+	help := "\nControls:"
+	help += "\nNew-File:Ctrl + N, List:Ctrl+L, Back/Save:ESC, Save:Ctrl+S, Quit:Ctrl+Q.\n"
+
+	help = style.Render(help)
+
+	// adding help to string s
+	s += help
 
 	// Send the UI for rendering
 	return tea.NewView(s)
